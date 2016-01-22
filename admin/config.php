b96aac00dab959096b79ef55daabd801a6b9052a
Sagar Borse <sagar.borse@justdevelop.it>
2016-01-22 14:43:05 +0530

    Opencart install

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://belagam.com/ocart/admin/');
+define('HTTP_CATALOG', 'http://belagam.com/ocart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://belagam.com/ocart/admin/');
+define('HTTPS_CATALOG', 'http://belagam.com/ocart/');
+
+// DIR
+define('DIR_ROOT', '/Applications/MAMP/htdocs/belagam/');
+define('DIR_APPLICATION', DIR_ROOT.'admin/');
+define('DIR_SYSTEM', DIR_ROOT.'system/');
+define('DIR_LANGUAGE', DIR_ROOT.'admin/language/');
+define('DIR_TEMPLATE', DIR_ROOT.'admin/view/template/');
+define('DIR_CONFIG', DIR_ROOT.'system/config/');
+define('DIR_IMAGE', DIR_ROOT.'image/');
+define('DIR_CACHE', DIR_ROOT.'system/cache/');
+define('DIR_DOWNLOAD', DIR_ROOT.'system/download/');
+define('DIR_UPLOAD', DIR_ROOT.'system/upload/');
+define('DIR_LOGS', DIR_ROOT.'system/logs/');
+define('DIR_MODIFICATION', DIR_ROOT.'system/modification/');
+define('DIR_CATALOG', DIR_ROOT.'catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123');
+define('DB_DATABASE', 'belagam_kart');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://belagam.com/ocart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://belagam.com/ocart/');
+
+// DIR
+define('DIR_ROOT', '/Applications/MAMP/htdocs/belagam/');
+define('DIR_APPLICATION', DIR_ROOT.'catalog/');
+define('DIR_SYSTEM', DIR_ROOT.'system/');
+define('DIR_LANGUAGE', DIR_ROOT.'catalog/language/');
+define('DIR_TEMPLATE', DIR_ROOT.'catalog/view/theme/');
+define('DIR_CONFIG', DIR_ROOT.'system/config/');
+define('DIR_IMAGE', DIR_ROOT.'image/');
+define('DIR_CACHE', DIR_ROOT.'system/cache/');
+define('DIR_DOWNLOAD', DIR_ROOT.'system/download/');
+define('DIR_UPLOAD', DIR_ROOT.'system/upload/');
+define('DIR_MODIFICATION', DIR_ROOT.'system/modification/');
+define('DIR_LOGS', DIR_ROOT.'system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123');
+define('DB_DATABASE', 'belagam_kart');
+define('DB_PREFIX', 'oc_');
