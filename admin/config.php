2be3c049770953ab189489ccc01fa6dc89f9cffc
Sagar Borse <sagar.borse@justdevelop.it>
2016-02-12 00:14:06 +0530

    Config changes

--- admin/config.php
+++ admin/config.php
@@ -1,30 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://belagam.local/admin/');
-define('HTTP_CATALOG', 'http://belagam.local/');
-
+define('HTTP_SERVER', 'http://belagam.com/admin/');
+define('HTTP_CATALOG', 'http://belagam.com/');
 // HTTPS
-define('HTTPS_SERVER', 'http://belagam.local/admin/');
-define('HTTPS_CATALOG', 'http://belagam.local/');
-
+define('HTTPS_SERVER', 'https://belagam.com/admin/');
+define('HTTPS_CATALOG', 'https://belagam.com/');
 // DIR
-define('DIR_APPLICATION', '/Applications/MAMP/htdocs/belagam/admin/');
-define('DIR_SYSTEM', '/Applications/MAMP/htdocs/belagam/system/');
-define('DIR_DATABASE', '/Applications/MAMP/htdocs/belagam/system/database/');
-define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/belagam/admin/language/');
-define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/belagam/admin/view/template/');
-define('DIR_CONFIG', '/Applications/MAMP/htdocs/belagam/system/config/');
-define('DIR_IMAGE', '/Applications/MAMP/htdocs/belagam/image/');
-define('DIR_CACHE', '/Applications/MAMP/htdocs/belagam/system/cache/');
-define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/belagam/download/');
-define('DIR_LOGS', '/Applications/MAMP/htdocs/belagam/system/logs/');
-define('DIR_CATALOG', '/Applications/MAMP/htdocs/belagam/catalog/');
-
+define('DIR_ROOT', '/home/websites/belagam/');
+define('DIR_APPLICATION', DIR_ROOT.'admin/');
+define('DIR_SYSTEM', DIR_ROOT.'system/');
+define('DIR_DATABASE', DIR_ROOT.'system/database/');
+define('DIR_LANGUAGE', DIR_ROOT.'admin/language/');
+define('DIR_TEMPLATE', DIR_ROOT.'admin/view/template/');
+define('DIR_CONFIG', DIR_ROOT.'system/config/');
+define('DIR_IMAGE', DIR_ROOT.'image/');
+define('DIR_CACHE', DIR_ROOT.'system/cache/');
+define('DIR_DOWNLOAD', DIR_ROOT.'download/');
+define('DIR_LOGS', DIR_ROOT.'system/logs/');
+define('DIR_CATALOG', DIR_ROOT.'catalog/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '123');
+define('DB_PASSWORD', 'Zerocool1$$');
 define('DB_DATABASE', 'belagam');
 define('DB_PREFIX', 'oc_');
 ?>
