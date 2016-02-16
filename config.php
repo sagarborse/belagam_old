4cb4fd7a6a9c122cd2bc05a8d7260cdb0681453d
root <root@belagam-1.c.oneall-site-417241-2.internal>
2016-02-16 18:35:27 +0000

    File added

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://www.belagam.com/');
+// HTTPS
+define('HTTPS_SERVER', 'https://www.belagam.com/');
+// DIR
+define('DIR_ROOT', '/home/websites/belagam/');
+define('DIR_APPLICATION', DIR_ROOT.'catalog/');
+define('DIR_SYSTEM', DIR_ROOT.'system/');
+define('DIR_DATABASE', DIR_ROOT.'system/database/');
+define('DIR_LANGUAGE', DIR_ROOT.'catalog/language/');
+define('DIR_TEMPLATE', DIR_ROOT.'catalog/view/theme/');
+define('DIR_CONFIG', DIR_ROOT.'system/config/');
+define('DIR_IMAGE', DIR_ROOT.'image/');
+define('DIR_CACHE', DIR_ROOT.'system/cache/');
+define('DIR_DOWNLOAD', DIR_ROOT.'download/');
+define('DIR_LOGS', DIR_ROOT.'system/logs/');
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '173.194.243.94');
+define('DB_USERNAME', 'belagam');
+define('DB_PASSWORD', 'Zerocool1$$');
+define('DB_DATABASE', 'belagam');
+define('DB_PREFIX', 'oc_');
+?>
